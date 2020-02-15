bb619cf1885c85067066356ee7fae040376e203a
22931 <2293121112@qq.com>
2020-02-15 16:41:19 +0800

    我的springboot项目初始化

---
+++ src/main/java/com/luoamin/Example.java
@@ -0,0 +1,23 @@
+package com.luoamin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by 22931 on 2020/2/15.
+ */
+@RestController
+@EnableAutoConfiguration
+public class Example {
+    @RequestMapping("/")
+    String home() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Example.class, args);
+    }
+
+}
